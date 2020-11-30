6cb98e7af6bd2775626f0ecc315a751a14ddcd28
Peter Wiley <peter.c.wiley@gmail.com>
2020-11-30 18:03:09 -0500

    dev: added custom fields and additional options to mix posts

--- wp-content/plugins/pipipi-post-types/pipipi-post-types.php
+++ wp-content/plugins/pipipi-post-types/pipipi-post-types.php
@@ -29,7 +29,7 @@ function pipipi_mixes_init() {
     'parent_item_colon'     => __( 'Parent Mixes:', 'textdomain' ),
     'not_found'             => __( 'No Mixes found.', 'textdomain' ),
     'not_found_in_trash'    => __( 'No Mixes found in Trash.', 'textdomain' ),
-    'featured_image'        => _x( 'Mix art', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+    'featured_image'        => _x( 'Mix Art', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
     'set_featured_image'    => _x( 'Set mix art', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
     'remove_featured_image' => _x( 'Remove mix art', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
     'use_featured_image'    => _x( 'Use as mix art', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
@@ -44,7 +44,7 @@ function pipipi_mixes_init() {
   $args = array(
     'description'         => __( 'Mixes', 'textdomain' ),
     'labels'              => $labels,
-    'supports'            => array( 'title', 'editor', 'author', 'revisions', 'custom-fields', ),
+    'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', 'custom-fields', ),
     'hierarchical'        => false,
     'public'              => true,
     'rewrite'             => array( 'slug' => 'mix' ),
@@ -104,3 +104,102 @@ function pipipi_register_taxonomy_artist() {
   register_taxonomy( 'artist', [ 'mix' ], $args );
 }
 add_action( 'init', 'pipipi_register_taxonomy_artist' );
+
+
+/**
+ * Add custom fields to mix post type
+ *
+ */
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+    'key' => 'group_5fc5754627b06',
+    'title' => 'Additional Mix Settings',
+    'fields' => array(
+        array(
+            'key' => 'field_5fc57555f7654',
+            'label' => 'Soundcloud Mix Link',
+            'name' => 'soundcloud_mix_link',
+            'type' => 'url',
+            'instructions' => 'URL of the Soundcloud mix',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+        ),
+        array(
+            'key' => 'field_5fc57584f7655',
+            'label' => 'Artist Link',
+            'name' => 'artist_link',
+            'type' => 'url',
+            'instructions' => 'URL of artist\'s page (can be Soundcloud, Bandcamp, etc.)',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+        ),
+        array(
+            'key' => 'field_5fc5773e2c34c',
+            'label' => 'Release Date',
+            'name' => 'release_date',
+            'type' => 'date_picker',
+            'instructions' => 'Original release date for the mix',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'display_format' => 'm/d/Y',
+            'return_format' => 'm/d/Y',
+            'first_day' => 1,
+        ),
+    ),
+    'location' => array(
+        array(
+            array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'mix',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'side',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+));
+
+endif;
+
+
+/**
+ * Modify 'add title' placeholder
+ *
+ */
+add_filter('enter_title_here', 'pipipi_mix_title_placeholder' , 20 , 2 );
+function pipipi_mix_title_placeholder($title , $post){
+
+    if( $post->post_type == 'mix' ){
+        $mix_title = "Add mix title";
+        return $mix_title;
+    }
+
+    return $title;
+
+}
